72347761e09d532f45a30f19857305df6015c0c3
ashur-k <ashurkanwal@yahoo.com>
2020-04-04 18:51:18 +0000

    callback function and working avengers API

---
+++ script.js
@@ -0,0 +1,19 @@
+function getData(cb) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("GET", "https://gateway.marvel.com/v1/public/comics?apikey=6ae880be7b37120c2896e366b41398b1");
+    xhr.send();
+
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+         cb(JSON.parse(this.responseText));
+       }
+    };
+}
+function printDataToConsole(data){
+    console.log(data);
+}
+getData(printDataToConsole);
+
+
+
